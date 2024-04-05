1b2ca1b5c8d72904f5263f12be5034c9376a448b
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2024-04-05 05:02:48 +0200

    modify pagination

--- Backend/store/mypagination.py
+++ Backend/store/mypagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class ProductPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 18
 
     def get_paginated_response(self, data):
         return Response({
