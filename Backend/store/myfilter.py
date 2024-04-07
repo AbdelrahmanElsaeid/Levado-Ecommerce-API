cf751be70383ae1e807772051d9e434ab34dc9b2
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2024-04-07 02:10:05 +0200

    update filter by name

--- Backend/store/myfilter.py
+++ Backend/store/myfilter.py
@@ -1,38 +1,22 @@
 from .models import Product
 from django_filters import rest_framework as filters
 import urllib.parse
-
+from .models import Category
 
 class ProductFilter(filters.FilterSet):
     color = filters.CharFilter(field_name='color_product__name', lookup_expr='exact')
+    brand__title = filters.CharFilter(field_name='brand__title', lookup_expr='exact')
+    category__title = filters.CharFilter(field_name='category__title', lookup_expr='exact')
+
+   
 
     class Meta:
         model = Product
         fields = {
-            'brand': ['in'],
+            #'brand': ['in'],
             "title": ["icontains"],
-            "category": ["exact"],
-            "brand": ["exact"],
+            #"category": ["exact"],
+            #"brand": ["exact"],
             "rating": ["exact"],
             "price": ["lte", "gte", "range"],
         }
-
-# from django_filters import rest_framework as filters
-# from .models import Product
-# from django.db.models import Q
-
-# class ProductFilter(filters.FilterSet):
-#     color = filters.CharFilter(field_name='color_product__name', lookup_expr='exact')
-
-#     def filter_brand_custom(self, queryset, name, value):
-#         brands = value.split(',')  # Split the comma-separated values
-#         q_objects = Q()
-#         for brand in brands:
-#             q_objects |= Q(**{f'{name}__exact': brand.strip()})
-#         return queryset.filter(q_objects)
-
-#     class Meta:
-#         model = Product
-#         fields = {
-#             'brand': [],  
-#         }
